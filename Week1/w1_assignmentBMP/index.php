8f9523aa419a248aaa3e8081281a45b4661d8eb0
bmpuellomartinez <87755973+bmpuellomartinez@users.noreply.github.com>
2022-04-04 09:46:02 -0400

    First assingment

---
+++ Week1/w1_assignmentBMP/index.php
@@ -0,0 +1,37 @@
+<?php
+
+$animals = [ 
+    'Elephant', 'Lion' , 'Leopard', 'Snake', 'Wolf', 'Monkey'
+];
+
+?>
+
+
+ 
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>w1 Mini Task</title>
+
+    <style>
+        ul{
+            background: #e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <ul>
+        <?php 
+        foreach($animals as $animal){
+            echo "<li>$animal</li>";
+        }
+        ?>
+   
+    </ul>  
+</body>
+</html>
