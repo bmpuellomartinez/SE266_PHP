5a38e1caadfd6f11aa99894e822f7a39783e440e
bmpuellomartinez <87755973+bmpuellomartinez@users.noreply.github.com>
2022-04-04 10:45:04 -0400

    Final update 4/4/2022

--- Week1/w1_assignmentBMP/index.php
+++ Week1/w1_assignmentBMP/index.php
@@ -1,5 +1,6 @@
 <?php
 
+//This is the array for the aninal. I created 5 animal.
 $animals = [ 
     'Elephant', 'Lion' , 'Leopard', 'Snake', 'Wolf', 'Monkey'
 ];
@@ -13,6 +14,12 @@ $animals = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>w1 Mini Task</title>
 
+    <!--Berta M. Puello-Martinez -->
+    <!--Instructor: Timothy Henry, PhD, PMP -->
+    <!--Course: PHP -->
+    <!--This Program will display five animals names. The code was 
+    absorbed from https://laracasts.com/series/php-for-beginners/episodes/6 -->
+
     <style>
         ul{
             background: #e3e3e3;
@@ -22,6 +29,7 @@ $animals = [
     </style>
 </head>
 <body>
+<!--This is the list of the array that goes through the foreach loop -->
     <ul>
         <?php 
         foreach($animals as $animal){
@@ -30,5 +38,6 @@ $animals = [
         ?>
    
     </ul>  
-</body>
+    <!--This is the end of the code -->
+</body> 
 </html>
