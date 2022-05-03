cb23eaab993b9179e363320d421f0c63131cce5f
bmpuellomartinez <87755973+bmpuellomartinez@users.noreply.github.com>
2022-05-02 21:34:13 -0400

    week 2 assignment

---
+++ Week2/w2_assignmentBMP1/index.php
@@ -0,0 +1,76 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta harsect="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"  href="style.css">
+    <title>Week2</title>
+</head>
+<body>
+
+<!-- Berta M. Puello-Martinez -->
+<!--Instructor: Timothy Henry, PhD, PMP -->
+<!--Course: PHP -->
+<!--Date: 5/2/2022 -->
+<!-- Absorbed from https://github.com/tmhvrml/se266-example-code/tree/94696a95da8a549492726d9ffc7676af5544d631/class_web_site/site -->
+
+<!-- Bootraps goes here -->
+    <h1>PHP - BERTA M. PUELLO-MARTINEZ</h1>
+<p>This are all my PHP assignment projects along with my code.
+</p>
+<h2>Assignment Overview</h2>
+
+<ul>
+    <li><a href="../../Week1/w1_assignmentBMP1">Week 1</a></li>
+    <li><a href="../../Week1/w1_assignmentBMP2">Week 2</a></li>
+    <li><a href="../../Week1/w1_assignmentBMP3">Week 3</a></li>
+    <li><a href="../../Week1/w1_assignmentBMP4">Week 4</a></li>
+    <li><a href="../../Week1/w1_assignmentBMP5">Week 5</a></li>
+    <li><a href="../../Week2/w2_assignmentBMP1">Week 6</a></li>
+    <li><a href="../week7/index.php">Week 7</a></li>
+    <li><a href="../week8/index.php">Week 8</a></li>
+    <li><a href="../week9/index.php">Week 9</a></li>
+    <li><a href="../week10/index.php">Week 10</a></li> 
+</ul>
+
+<!-- Adding the Resources links to PHP -->
+<h2>PHP Resources</h2>
+       
+<ul>
+    <li><a href="https://www.php.net/manual/en/language.types.resource.php">PHP Resources Manual</a></li> 
+    <li><a href= "https://www.tutorialspoint.com/php-resources">PHP Tutorials</a></li> 
+    <li><a href= "https://stackoverflow.com/questions/38429595/php-7-and-strict-resource-types">PHP-Resources Stack overflow</a></li> 
+</ul> 
+<!-- end -->
+
+<!--Here are the links of resources provided to search git information.  -->
+<h2>Git Resourecs</h2>
+<ul>
+    <li><a href="https://www.w3schools.com/git/">Git Tutorials w3schools</a></li> 
+    <li><a href= "https://git-scm.com/book">ONLINE- BOOK</a></li> 
+    <li><a href= "https://www.atlassian.com/git">Git-Tutorials Stack overflow</a></li> 
+</ul>
+<!-- end -->
+
+<h2>Three links of my working labs</h2>
+<ul>
+    <li><a href="https://www.w3schools.com/git/">week #1</a></li> 
+    <li><a href= "https://git-scm.com/book">Week#2</a></li> 
+    <li><a href= "https://www.atlassian.com/git">Week #3</a></li> 
+</ul>
+
+<!--Here are the links of the things that I like to do outside of school  -->
+<h2>3 Things I enjoy to do!</h2>
+<ul>
+    <li><a href="https://www.buttonwoodfarmicecream.com/">I enjoy eating ice-cream at ButtonWood farm!</a></li> 
+    <li><a href= "https://riparks.com/beach/misquamicut.php">I enjoy going to Masquamicut beach!</a></li> 
+    <li><a href= "https://www.midosushinorwich.com/">I like eating Sushi!</a></li> 
+</ul>
+
+<!-- Here is the footer link -->
+<?php include __DIR__ . '/footer.php'; ?>
+
+</body>
+</html>
